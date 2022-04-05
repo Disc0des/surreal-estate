18c6c91ff53be3d6b7e19bb8ad3b220b7a7435f1
DanHembery <93157414+DanHembery@users.noreply.github.com>
2022-04-05 13:25:29 +0100

    Initial commit

---
+++ src/tests/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from '../components/App';
+
+describe("App", () => {
+  it("renders App component correctly", () => {
+    const { asFragment } = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
